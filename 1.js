89ff7579e3d145fe811da089aa44693978690670
Praveen Dias <praveendias1180@gmail.com>
2022-04-18 23:24:41 +0530

    _.get

--- 1.js
+++ 1.js
@@ -10,3 +10,29 @@ _.forEach(users, function (user, index) {
 });
 
 console.log(users)
+
+const user = {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "brad",
+      "last": "gibson"
+    },
+    "location": {
+      "street": "9278 new road",
+      "city": "kilcoole",
+      "state": "waterford",
+      "postcode": "93027",
+      "coordinates": {
+        "latitude": "20.9267",
+        "longitude": "-7.9310"
+      },
+      "timezone": {
+        "offset": "-3:30",
+        "description": "Newfoundland"
+      }
+    }
+};
+
+console.log(_.get(user, 'location.timezone', {}));
+console.log(_.get(user, 'name.last', ''));
